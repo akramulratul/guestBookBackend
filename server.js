34ee1ac3d05ed4bafe889fa6107fd0c2afbfbb3a
Akramul Ratul <akramulratul@gmail.com>
2023-07-20 19:59:22 +0600

    cors problem solve

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+// Use CORS middleware to handle CORS preflight requests
+app.use(cors());
+
+app.get("/api/v1/search/typeahead", (req, res) => {
+  const query = req.query.query;
+
+  axios
+    .get(
+      `https://theguestbook.com/api/v1/search/typeahead?search_page_id=12&query=${query}`
+    )
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.status(500).send({ error: error.message });
+    });
+});
+
+app.get("/api/v1/search/get_coords", (req, res) => {
+  const { search_type, query } = req.query;
+  axios
+    .get(
+      `https://theguestbook.com/api/v1/search/get_coords?search_type=City&query=${query}`
+    )
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send();
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
